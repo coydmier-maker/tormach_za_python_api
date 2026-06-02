1c8196c4337904f30b38fa3cb053983ef8217879
coydmier-maker <coydmier@gmail.com>
2026-06-02 15:44:59 -0500

    bentover

--- moveit_first_architecture/second_irl_test.py
+++ moveit_first_architecture/second_irl_test.py
@@ -40,7 +40,7 @@ def main():
         time.sleep(2.0)
 
         robot.set_speed(0.025)
-        robot.move_to_named_pose("plankPosition")
+        robot.move_to_named_pose("bentOver")
         time.sleep(2.0)
         robot.move_to_named_pose("uppies")
         time.sleep(2.0)
