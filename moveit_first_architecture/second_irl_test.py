c0b5b34cebdb8cfb010a5c8e6d13bd8b9f3d3d9c
coydmier-maker <coydmier@gmail.com>
2026-06-02 15:00:31 -0500

    uppies

--- moveit_first_architecture/second_irl_test.py
+++ moveit_first_architecture/second_irl_test.py
@@ -40,7 +40,7 @@ def main():
         time.sleep(2.0)
 
         robot.set_speed(0.025)
-        robot.move_to_named_pose("tall")
+        robot.move_to_named_pose("uppies")
         time.sleep(2.0)
         robot.move_to_named_pose("home")
 
