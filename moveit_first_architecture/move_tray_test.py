c782ce7afe420d05ecb0bb048392999ae016a9ab
coydmier-maker <coydmier@gmail.com>
2026-06-04 14:51:04 -0500

    prep joints

--- moveit_first_architecture/move_tray_test.py
+++ moveit_first_architecture/move_tray_test.py
@@ -39,7 +39,7 @@ def main():
         time.sleep(2.0)
 
         robot.set_speed(0.025)
-        #robot.move_to_named_pose("preparing")
+        robot.move_to_named_pose("preparingJoints")
         time.sleep(1.0)
         robot.gripper.open()
         time.sleep(1.0)
